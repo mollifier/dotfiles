cd04416f713d33a9415d1137bf50606c9b0d5d1e
mollifier <mollifier@gmail.com>
2026-05-12 18:03:32 +0900

    Add Node.js

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -20,7 +20,8 @@ if [ ! -d ~/.config/anyenv/anyenv-install ]; then
 fi
 
 ~/.anyenv/bin/anyenv install --skip-existing nodenv
-
 eval "$(~/.anyenv/bin/anyenv init - bash)"
 ~/.anyenv/envs/nodenv/bin/nodenv install --skip-existing 24.13.1
+~/.anyenv/envs/nodenv/bin/nodenv install --skip-existing 24.15.0
+~/.anyenv/envs/nodenv/bin/nodenv global 24.15.0
 
