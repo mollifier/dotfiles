76c1eb4b30865faf9171c341966c60094ecca080
mollifier <mollifier@gmail.com>
2026-05-12 13:51:54 +0900

    Add fisher setup script

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# fisher
+# https://github.com/jorgebucaran/fisher
+fish -c 'fisher update'
+
 # tpm(Tmux Plugin Manager)
 # https://github.com/tmux-plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
