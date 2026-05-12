6204a0339e2fa42536324dce69d33066cfdd2a7f
mollifier <mollifier@gmail.com>
2026-05-12 18:12:15 +0900

    Settings for vim undo persistence

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Vim
 # directory for vim backup file
 if [ ! -d ~/.local/share/vim/backup ]; then
   mkdir -p ~/.local/share/vim/backup
+fi
+# Undo persistence
+if [ ! -d ~/.local/share/vim/undo ]; then
+  mkdir -p ~/.local/share/vim/undo
 fi
 
 # fisher
