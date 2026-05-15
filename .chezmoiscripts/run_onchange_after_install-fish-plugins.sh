96e30b7b21e1f133d08794adf565b3869c5fee0a
mollifier <mollifier@gmail.com>
2026-05-15 14:45:32 +0900

    Put EOF

--- .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
+++ .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
@@ -4,7 +4,6 @@
 # https://github.com/jorgebucaran/fisher
 if command -v fish >/dev/null 2>&1; then
   if [ -f ~/.config/fish/functions/fisher.fish ]; then
-    echo 'instal fish plugins'
-    fish -c 'set fish_trace 1; source ~/.config/fish/functions/fisher.fish; fisher update'
+    fish -c 'source ~/.config/fish/functions/fisher.fish; fisher update' < /dev/null
   fi
 fi
