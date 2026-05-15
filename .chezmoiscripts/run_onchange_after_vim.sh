606376af9d9e1fb7645e8af654de389df330812c
mollifier <mollifier@gmail.com>
2026-05-15 13:20:27 +0900

    Add after vim script

---
+++ .chezmoiscripts/run_onchange_after_vim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# directory for vim backup file
+if [ ! -d ~/.local/share/vim/backup ]; then
+  mkdir -p ~/.local/share/vim/backup
+fi
+
+# Undo persistence
+if [ ! -d ~/.local/share/vim/undo ]; then
+  mkdir -p ~/.local/share/vim/undo
+fi
