c771f654c98afcdd458b873e93ede8bb609dcdc5
mollifier <mollifier@gmail.com>
2026-05-15 14:47:33 +0900

    Don't source fisher

--- .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
+++ .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
@@ -4,6 +4,6 @@
 # https://github.com/jorgebucaran/fisher
 if command -v fish >/dev/null 2>&1; then
   if [ -f ~/.config/fish/functions/fisher.fish ]; then
-    fish -c 'source ~/.config/fish/functions/fisher.fish; fisher update' < /dev/null
+    fish -c 'fisher update' < /dev/null
   fi
 fi
