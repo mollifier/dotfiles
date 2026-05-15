5aa1ddf9332fce831b16d2ba5672fd3044519222
mollifier <mollifier@gmail.com>
2026-05-15 14:31:18 +0900

    Add debug message

--- .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
+++ .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
@@ -4,6 +4,7 @@
 # https://github.com/jorgebucaran/fisher
 if command -v fish >/dev/null 2>&1; then
   if [ -f ~/.config/fish/functions/fisher.fish ]; then
+    echo 'instal fish plugins'
     fish -c "source ~/.config/fish/functions/fisher.fish; fisher update"
   fi
 fi
