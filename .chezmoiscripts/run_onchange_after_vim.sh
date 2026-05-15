8a6b71d89279eee9cf834eda5966b5edac5a42f7
mollifier <mollifier@gmail.com>
2026-05-15 15:14:34 +0900

    Install vim plugins

--- .chezmoiscripts/run_onchange_after_vim.sh
+++ .chezmoiscripts/run_onchange_after_vim.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if ! command -v vim >/dev/null 2>&1; then
+  exit 0
+fi
+
 # directory for vim backup file
 if [ ! -d ~/.local/share/vim/backup ]; then
   mkdir -p ~/.local/share/vim/backup
@@ -8,4 +12,11 @@ fi
 # Undo persistence
 if [ ! -d ~/.local/share/vim/undo ]; then
   mkdir -p ~/.local/share/vim/undo
+fi
+
+# vim-plug
+# https://github.com/junegunn/vim-plug
+if [ -f ~/.vimplug.vim ]; then
+  # Treat as success on forced periodic execution.
+  vim -es -u ~/.vimplug.vim -i NONE -c 'PlugInstall' -c 'qa' || true
 fi
