23519bae5cf76d6aa81ef4c52a4e0ae1d8d83f39
mollifier <mollifier@gmail.com>
2026-05-15 14:37:27 +0900

    Add trace output

--- .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
+++ .chezmoiscripts/run_onchange_after_install-fish-plugins.sh
@@ -5,6 +5,6 @@
 if command -v fish >/dev/null 2>&1; then
   if [ -f ~/.config/fish/functions/fisher.fish ]; then
     echo 'instal fish plugins'
-    fish -c "source ~/.config/fish/functions/fisher.fish; fisher update"
+    fish -c 'set fish_trace 1; source ~/.config/fish/functions/fisher.fish; fisher update'
   fi
 fi
